ba45d1c0b5b6da46fe94ea8665c4a08c67f0380a
Amplify Rebel <erj2204@gmail.com>
2022-04-24 22:21:53 -0500

    added functions and values

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json());
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
+    port: 3001,
     user: 'root',
     database: 'test'
 });
@@ -64,13 +65,13 @@ function startup() {
 };
 function viewEmployees() {
     var query = 'SELECT * FROM employee';
-    connection.query(query); {
+    connection.query(query, function(res) {
+        
         console.log(res.length + ' employees found!');
         console.table('All Employees:', res); 
         options();
     })
-};
-
+}
 function updateRole() {
 
 };
@@ -84,9 +85,148 @@ function deleteEmployee() {
 function close() {
     connection.end();
 };
-function addRole() 
-function viewDepartments()
-function addDepartment() 
-function addEmployee()
-function viewRoles()
-function viewEmployees()
+function addRole() {
+    connection.query('SELECT * FROM department', function(err, res) {
+        if (err) throw err;
+    
+        inquirer 
+        .prompt([
+            {
+                name: 'new_role',
+                type: 'input', 
+                message: "What new role would you like to add?"
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'What is the salary of this role? (Enter a number)'
+            },
+            {
+                name: 'Department',
+                type: 'list',
+                choices: function() {
+                    var deptArry = [];
+                    for (let i = 0; i < res.length; i++) {
+                    deptArry.push(res[i].name);
+                    }
+                    return deptArry;
+                },
+            }
+        ]).then(function (answer) {
+            let department_id;
+            for (let a = 0; a < res.length; a++) {
+                if (res[a].name == answer.Department) {
+                    department_id = res[a].id;
+                }
+            }
+    
+            connection.query(
+                'add new role?',
+                {
+                    title: answer.new_role,
+                    salary: answer.salary,
+                    department_id: department_id
+                },
+                function (res) {
+                    console.log('Your new role has been added!');
+                    console.table('All Roles:', res);
+                    options();
+                })
+        })
+    })
+}; 
+
+function viewDepartments(){
+    var query = 'SELECT * FROM department';
+    connection.query(query, function(res) {
+        
+        console.table('All Departments:', res);
+        options();
+    })
+};
+
+function addDepartment() {
+    inquirer
+        .prompt([
+            {
+                name: 'addDepartment', 
+                type: 'input', 
+                message: 'What department would you like to add?'
+            }
+            ]).then(function (answer) {
+                connection.query(
+                    'confirm new department?',
+                    {
+                        name: answer.newDepartment
+                    });
+                var query = 'SELECT * FROM department';
+                connection.query(query, function( res) {
+              
+                console.log('Your department has been added!');
+                console.table('All Departments:', res);
+                options();
+                })
+            })
+};
+
+function addEmployee() {
+    connection.query('SELECT * FROM role', function (res) {
+        inquirer
+            .prompt([
+                {
+                    name: 'first_name',
+                    type: 'input', 
+                    message: "What is the employee's fist name? ",
+                },
+                {
+                    name: 'last_name',
+                    type: 'input', 
+                    message: "What is the employee's last name? "
+                },
+                {
+                    name: 'manager_id',
+                    type: 'input', 
+                    message: "What is the employee's manager's ID? "
+                },
+                {
+                    name: 'role', 
+                    type: 'list',
+                    choices: function() {
+                    var roleArray = [];
+                    for (let i = 0; i < res.length; i++) {
+                        roleArray.push(res[i].title);
+                    }
+                    return roleArray;
+                    },
+                    message: "What is this employee's role? "
+                }
+                ]).then(function (answer) {
+                    let role_id;
+                    for (let a = 0; a < res.length; a++) {
+                        if (res[a].title == answer.role) {
+                            role_id = res[a].id;
+                            console.log(role_id)
+                        }                  
+                    }  
+                    connection.query(
+                    ' add new emplouyee?',
+                    {
+                        first_name: answer.first_name,
+                        last_name: answer.last_name,
+                        manager_id: answer.manager_id,
+                        role_id: role_id,
+                    },
+                    function () {
+                        console.log('Your employee has been added!');
+                        options();
+                    })
+                })
+        })
+};
+function viewRoles() {
+    var query = 'SELECT * FROM role';
+    connection.query(query, function(res){
+        console.table('All Roles:', res);
+        options();
+    })
+};
--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,8 +1,24 @@
 INSERT INTO department (name)
 VALUES 
+('Information Systems and Technology'),
+('Finance'),
+('Legal'),
+('Human Resources'),
+('Security'),
+('Sales');
 
 INSERT INTO role (title, salary, department_id)
 VALUES
+('Lawyer', 120000, 1),
+('Accountant', 65000, 2),
+('Engineer', 110000, 3),
+('Manager', 77000, 4),
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  
+VALUES 
+('Johnnie', 'Johnson', 1, 2), 
+('Dave', 'Smith', 1, null), 
+('Eli', 'Manning', 1, 2),
+ ('Mark', 'Jones', 2, 2), 
+ ('Ethan', 'Johnson', 4, null);
